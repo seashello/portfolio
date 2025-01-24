d2490efa4f67b348d7f61f40253c941619277931
seashello <164724311+seashello@users.noreply.github.com>
2025-01-23 16:21:17 -0800

    saved global file

--- global.js
+++ global.js
@@ -4,4 +4,26 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+// const navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: "Contact" },
+    { url: 'resume/', title: "Resume" },
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
