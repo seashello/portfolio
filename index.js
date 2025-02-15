bf6326319d108899b9df72e297caf881731d720f
seashello <164724311+seashello@users.noreply.github.com>
2025-02-14 21:21:03 -0800

    changed direct

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { fetchJSON, renderProjects, fetchGitHubData } from '../global.js';
-const projects = await fetchJSON('../lib/projects.json');
+import { fetchJSON, renderProjects, fetchGitHubData } from "./global.js";
+const projects = await fetchJSON("../lib/projects.json");
 const latestProjects = projects.slice(0, 3);
-const projectsContainer = document.querySelector('.projects');
-renderProjects(latestProjects, projectsContainer, 'h2');
+const projectsContainer = document.querySelector(".projects");
+renderProjects(latestProjects, projectsContainer, "h2");
 
-const githubData = await fetchGitHubData('seashello');
-const profileStats = document.querySelector('#profile-stats');
+const githubData = await fetchGitHubData("seashello");
+const profileStats = document.querySelector("#profile-stats");
 if (profileStats) {
-    profileStats.innerHTML = `
+  profileStats.innerHTML = `
         <dl>
             <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
             <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
